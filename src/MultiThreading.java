ff1fb467c9e966303019f2edeb027fb509b48c0e
Falchio <soulsard@gmail.com>
2019-12-04 22:58:51 +0300

    Four Thread

--- src/MultiThreading.java
+++ src/MultiThreading.java
@@ -1,11 +1,11 @@
-
+import java.util.Arrays;
 
 public class MultiThreading {
 
 
     public static void main(String[] args) {
         firstMethod(); //без разбиения массива
-        secondMethod(); //два потока
+        secondMethod(); //четыре потока
     }
 
 
@@ -33,11 +33,12 @@ public class MultiThreading {
     public static void secondMethod() {
 
         final int size = 10000000;
-        final int h = size / 2;
+        final int h = size / 4;
         float[] arr = new float[size];
         float[] a1 = new float[h];
         float[] a2 = new float[h];
-
+        float[] a3 = new float[h];
+        float[] a4 = new float[h];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 1;
@@ -65,21 +66,41 @@ public class MultiThreading {
             System.arraycopy(a2, 0, arr, h, h);
         });
 
+        Thread A3 = new Thread(()->{
+            System.arraycopy(arr, h*2, a3, 0, h);
+            for (int i = 0; i < a3.length; i++) {
+                a3[i] = (float) (a3[i] * Math.sin(0.2f + i / 5)
+                        * Math.cos(0.2f + i / 5)
+                        * Math.cos(0.4f + i / 2));
+            }
+            System.arraycopy(a3, 0, arr, h*2, h);
+        });
+
+        Thread A4 = new Thread(()->{
+            System.arraycopy(arr, h*3, a4, 0, h);
+            for (int i = 0; i < a4.length; i++) {
+                a4[i] = (float) (a4[i] * Math.sin(0.2f + i / 5)
+                        * Math.cos(0.2f + i / 5)
+                        * Math.cos(0.4f + i / 2));
+            }
+            System.arraycopy(a4, 0, arr, h*3, h);
+        });
 
         A1.start();
         A2.start();
+        A3.start();
+        A4.start();
 
         try {
             A1.join();
             A2.join();
+            A3.join();
+            A4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-
         System.out.println("Метод с разбивкой массивов отработал за "+ (System.currentTimeMillis() - timeStart) +" миллисекунд(ы)");
     }
-
-
 
 }
